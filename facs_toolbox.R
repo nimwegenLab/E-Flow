f8d6f9ffa4887a048c38130a869afecae3b46c4a
urchuegu <arantxa.urchueguia@unibas.ch>
2015-01-22 14:29:00 +0100

    changed file path from reading_gene_name_function, since it was not working before, but it does now

--- facs_toolbox.R
+++ facs_toolbox.R
@@ -484,7 +484,7 @@ propagate_index_info <- function(.pls, .info) {
 #The file is located in the facs_toolbox and the .join variable refers to to which dataframe of the final list you
 #want to add the information about the genes. 
 read_combine_gene_name_file <- function(file ="~/projects/facs_toolbox/alon_all_strains_forweb.csv", .join){
-        alon_pwg <- read.csv(alonpwg_path) %>%
+        alon_pwg <- read.csv(file) %>%
                 select (Plate_Number, Well, Gene_Name) %>%
                 rename(plate_upper=Plate_Number, well=Well, gene=Gene_Name)   
         
