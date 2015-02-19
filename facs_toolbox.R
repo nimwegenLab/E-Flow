d17273372da44dcd919282bba4f2acbff37af68d
Thomas Julou <thomas.julou@gmail.com>
2015-02-19 20:20:01 +0100

    Add min_noise_manual_fit to fit parameters of the minimal noise function interactively.

--- facs_toolbox.R
+++ facs_toolbox.R
@@ -30,8 +30,7 @@ scale_shape_periodic <-
   function(...) scale_shape_manual(..., values = rep(15:18, 5))
 
 
-### FUNCTIONS
-
+### PREPROCESSING FUNCTIONS ####
 split_well_col <- function(.df, .col='well', .override=FALSE) {
 # split_well_col splits the column named after .col to `row` and `col`
   if ('row' %in% names(.df) || 'col' %in% names(.df)) {
@@ -558,7 +557,99 @@ read_combine_gene_name_file <- function(file, .df_join){
         left_join(.df_join, alon_pwg, by = c("plate", "well"))
 }
 
+
+### ANALYSIS FUNCTIONS ####
+
+mean_log10_to_ln <- function(.m)
+  return(.m / log10(exp(1)))
+
+var_log10_to_ln <- function(.v)
+  return(.v / log10(exp(1))^2)
+
+min_noise_vs_n <- function(.sigma2_ab, .beta, .n_bg) {
+# minimal noise relationship as defined in Wolf, et al. 2014 (eqn 66, http://dx.doi.org/10.1101/007237).
+  function(.n) ifelse(.n > 2*.n_bg, .sigma2_ab * (1 - .n_bg/.n)^2 + .beta/.n * (1 - .n_bg/.n), NA)
+}
+
+min_noise_vs_n_ln <- function(.sigma2_ab, .beta, .n_bg) {
+# minimal noise relationship derived from Wolf, et al. 2014 (eqn 66, http://dx.doi.org/10.1101/007237).
+  function(.n_ln) (min_noise_vs_n(.sigma2_ab, .beta, .n_bg))(exp(.n_ln))
+}
+
+min_noise_manual_fit <- function(.mean_ln, .var_ln, .m_bg=NA, .bsize=NA, .extr_noise=NA,
+                                 .gfp_per_FCMunit=2.884) {
+# min_noise_manual_fit helps the user choosing parameter values to fit manually
+# the minimal noise relationship as defined in Wolf, et al. 2014 (eqn 66, http://dx.doi.org/10.1101/007237).
+  stopifnot(length(.mean_ln) == length(.var_ln))
+  cat("\nHint: make sure that your data are transformed with ln and not log10...")
+  
+#   $corvar{$gene} = $s[4]-450*(1.0-582.36/(2.884*exp($s[3])))/(2.884*exp($s[3]))-0.025*(1.0-582.36/(2.884*exp($s[3])))*(1.0-582.36/(2.884*exp($s[3])));
+  
+  if (is.na(.m_bg)) {
+    cat("\nPlease enter your estimation of background fluo ln mean (e.g. from empty plasmid measurements):")
+    .m_bg <- scan(n=1, what=numeric())
+  }
+  .n_bg <- .gfp_per_FCMunit * .m_bg
+  .n_ln <- log( .gfp_per_FCMunit * exp(.mean_ln) - .n_bg )
+
+  if (is.na(.extr_noise)) {
+    value_set <- FALSE
+    while (!value_set) {
+      cat("\nPlease enter your estimation of extrinsic noise (i.e. var at high mean levels):")
+      .extr_noise <- scan(n=1, what=numeric())
+      
+      pl <- qplot(.n_ln, .var_ln) + 
+        geom_hline(yintercept=.extr_noise, col='red') +
+        expand_limits(y=0)
+      print(pl)
+      
+      cat("\nIs this plot correct (y or n)?\n")
+      y <- scan(n=1, what=character())
+      if (y == "y") value_set <- TRUE
+    }
+  }
+  
+  if (is.na(.bsize)) {
+    value_set <- FALSE
+    while (!value_set) {
+      cat("\nPlease enter your estimation of transcription burst size:")
+      cat("\n(yes, you must guess! 400 might be a good start :)")
+      cat("\nHint: you can enter several values to see them plotted all at once...")
+      .bsize <- scan(what=numeric())
+      
+#       min_noise_b <- function(.b) min_noise_vs_n_ln(.extr_noise, .b, .n_bg)
+#       pl <- qplot(.n_ln, .var_ln) + expand_limits(y=0)
+#       for (.b in .bsize) 
+#         pl <- pl + stat_function(aes(col=factor(.b)), fun=min_noise_b(.b))
+#       print(pl + labs(col="burst size"))
+      .xs <- seq(5, 13, length.out=1000)
+      myfns <- data.frame(bsize=.bsize) %>%
+        group_by(bsize) %>%
+        do((function(.df) data.frame(n_ln=.xs, var_ln=(min_noise_vs_n_ln(.extr_noise, .df$bsize, .n_bg))(.xs)))(.))
+  
+      pl <- ggplot(data=NULL, aes(x=n_ln, y=var_ln)) + expand_limits(y=0) +
+        geom_point(data=data.frame(n_ln=.n_ln, var_ln=.var_ln), size=.5) +
+        geom_line(aes(col=factor(bsize)), data=myfns, size=1)
+      print(pl)  
+
+      cat("\nIs this plot correct (y or n)?\n")
+      y <- scan(n=1, what=character())
+      if (y == "y") {
+        if (length(.bsize) > 1) {
+          cat("\nPlease choose a single value of transcription burst size before validating.")
+        } else { 
+          value_set <- TRUE
+        }
+      }
+    }
+  }
+  
+  .excess_noise <- .var_ln - (min_noise_vs_n_ln(.extr_noise, .bsize, .n_bg))(.n_ln)
+  return(list(m_bg=.m_bg, bsize=.bsize, extr_noise=.extr_noise, n_ln=.n_ln, excess_noise=.excess_noise))
+}
+
 d_sum_norm <- function(.x, .means, .sds) {
+# d_sum_norm returns the density of the sum of normal RV with parameters (.means, .sds)
   if (length(.means) != length(.sds)) 
     stop('.means and .sds must have the same length.')
   lapply(seq_along(.means), function(.i) dnorm(.x, .means[.i], .sds[.i])) %>%
